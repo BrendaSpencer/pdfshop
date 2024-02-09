66a53e5bcb7e0686f091e14057fee29dd03cf547
BrendaSpencer <spencerbernadette1@gmail.com>
2024-02-09 12:34:52 +0100

    adding twig

--- src/Controller/PDFController.php
+++ src/Controller/PDFController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
 use function Symfony\Component\String\u;
+use Symfony\Component\HttpFoundation\Response;
 
-class PDFController 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PDFController extends AbstractController
 {
     #[Route('/')]
     public function homepage():Response
     {
-       return new Response('Hai , Theeerrre');
+        return $this->render('pdf/homepage.html.twig', [
+            'title' => 'PDF Shop',
+        ]);
     }
 
     #[Route('/browse/{slug}')]
